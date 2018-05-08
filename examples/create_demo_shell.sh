e31e1ac7dd5e3fa853de2cc9f045cff85895bcaf
simonis <volker.simonis@gmail.com>
2018-05-09 01:51:33 +0200

    General version just before GeeCON2018/Krakow

--- examples/create_demo_shell.sh
+++ examples/create_demo_shell.sh
@@ -1,19 +1,39 @@
 
 export PS1="\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w\[\e[0m\]\n\$ "
 
-export PATH=/share/output-jdk9-hs-comp-dbg/images/jdk/bin:$PATH
+CWD=`pwd`
 
-export _JAVA_OPTIONS='-Xbatch -XX:-UseCompressedOops -XX:+UseSerialGC -XX:-TieredCompilation -XX:-UseOnStackReplacement -XX:+UnlockDiagnosticVMOptions -XX:-CheckIntrinsics -XX:-LogVMOutput -XX:CICompilerCount=2'
+export PATH=/share/output-jdk-dbg/images/jdk/bin:$PATH
+
+export _JAVA_OPTIONS='-Xbatch -XX:-UseCompressedOops -XX:+UseSerialGC -XX:-TieredCompilation -XX:-UseOnStackReplacement -XX:+UnlockDiagnosticVMOptions -XX:-CheckIntrinsics -XX:-LogVMOutput -XX:CICompilerCount=1'
 
 if [ "$1" == "random_print" ]; then
   export LD_LIBRARY_PATH=/share/OpenJDK/hsdis
+else
+if [ "$1" == "loop" ]; then
+  export _JAVA_OPTIONS='-Xbatch -XX:-UseCompressedOops -XX:+UseSerialGC -XX:-TieredCompilation -XX:-UseOnStackReplacement -XX:+UnlockDiagnosticVMOptions -XX:-LogVMOutput -XX:CICompilerCount=1'
+else
+if [ "$1" == "loop_print" ]; then
+  export _JAVA_OPTIONS='-Xbatch -XX:-UseCompressedOops -XX:+UseSerialGC -XX:-TieredCompilation -XX:-UseOnStackReplacement -XX:+UnlockDiagnosticVMOptions -XX:-LogVMOutput -XX:CICompilerCount=1'
+else
+if [ "$1" == "loop_with_gc" ]; then
+  export _JAVA_OPTIONS='-Xbatch -XX:-UseCompressedOops -XX:+UseSerialGC -XX:-TieredCompilation -XX:-UseOnStackReplacement -XX:+UnlockDiagnosticVMOptions -XX:-LogVMOutput -XX:CICompilerCount=1'
+fi
+fi
+fi
 fi
 
-alias javac=/share/output-jdk9-hs-comp-opt/images/jdk/bin/javac
+alias javac=/share/software/Java/jdk1.8.0_172/bin/javac
+alias java9=/share/output-jdk9-dev_ObjectLayout-dbg/images/jdk/bin/java
+alias la='ls -la'
 
-cd /c/Users/D046063/public_html/hotspot/JET2016/git/examples/bin
+rm -rf /tmp/Demo_$1
+mkdir -p /tmp/Demo_$1
+cd /tmp/Demo_$1
+
+export CLASSPATH=$CWD/bin
 
 set -o history
 unset HISTFILE
 history -c
-history -r ../.history_$1
+history -r $CWD/.history_$1
--- examples/src/Readme.txt
+++ examples/src/Readme.txt
@@ -1,6 +1,7 @@
 Howto build:
 
-/share/software/Java/jdk1.8.0_66/bin/javac -g -d ../bin/ -XDignore.symbol.file=true io/simonis/*.java
-/share/output-jdk9-hs-comp-dbg/images/jdk/bin/jar cvfm ../bin/MethodInstrumentationAgent.jar manifest.mf ../bin/io/simonis/MethodInstrumentationAgent*.class
+mkdir -p ../bin
+/share/software/Java/jdk1.8.0_172/bin/javac -g -d ../bin/ -XDignore.symbol.file=true io/simonis/*.java
+/share/software/Java/jdk1.8.0_172/bin/jar cvfm ../bin/MethodInstrumentationAgent.jar manifest.mf ../bin/io/simonis/MethodInstrumentationAgent*.class
 
-g++ -fPIC -shared -I /share/output-jdk9-hs-comp-dbg/images/jdk/include/ -I /share/output-jdk9-hs-comp-dbg/images/jdk/include/linux/ -o ../bin/traceMethodAgent.so jvmti/traceMethodAgent.cpp
+g++ -fPIC -shared -I /share/output-jdk-dbg/images/jdk/include/ -I /share/output-jd-dbg/images/jdk/include/linux/ -o ../bin/traceMethodAgent.so jvmti/traceMethodAgent.cpp
